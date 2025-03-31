84704af18dc83e44ee3b966ea00d207d80561a4d
basiagaoskar <basiagaoskar@gmail.com>
2025-03-31 22:59:14 +0200

    Fix CI file and attach implementation

--- src/UE/Tests/Application/ApplicationTestSuite.cpp
+++ src/UE/Tests/Application/ApplicationTestSuite.cpp
@@ -11,12 +11,14 @@
 
 namespace ue
 {
-using namespace ::testing;
+    using namespace ::testing;
+    using namespace std::chrono_literals;
 
 class ApplicationTestSuite : public Test
 {
 protected:
     const common::PhoneNumber PHONE_NUMBER{112};
+    const common::BtsId BTS_ID{22};
     NiceMock<common::ILoggerMock> loggerMock;
     StrictMock<IBtsPortMock> btsPortMock;
     StrictMock<IUserPortMock> userPortMock;
@@ -30,10 +32,28 @@ protected:
 };
 
 struct ApplicationNotConnectedTestSuite : ApplicationTestSuite
-{};
-
-TEST_F(ApplicationNotConnectedTestSuite, todo)
 {
+    void shallHandleSibMessage()
+     {
+         EXPECT_CALL(btsPortMock, sendAttachRequest(BTS_ID));
+         EXPECT_CALL(timerPortMock, startTimer(500ms));
+         EXPECT_CALL(userPortMock, showConnecting());
+ 
+         objectUnderTest.handleSib(BTS_ID);
+     }
+};
+
+TEST_F(ApplicationNotConnectedTestSuite, shallHandleSibMessage)
+{  
+    shallHandleSibMessage();
 }
+
+struct ApplicationConnectingTestSuite : ApplicationNotConnectedTestSuite
+ {
+     ApplicationConnectingTestSuite()
+     {
+         shallHandleSibMessage();
+     }
+ };
 
 }
