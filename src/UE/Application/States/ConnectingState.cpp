93dd3ff8fb1d1160684d88fb776560e4d6e69222
WoyTuloO <wojciech.blicharz192@gmail.com>
2025-03-31 15:43:53 +0200

    added attach

--- src/UE/Application/States/ConnectingState.cpp
+++ src/UE/Application/States/ConnectingState.cpp
@@ -1,11 +1,20 @@
 #include "ConnectingState.hpp"
-
+#include "ConnectedState.hpp"
 namespace ue
 {
 
 ConnectingState::ConnectingState(Context &context)
     : BaseState(context, "ConnectingState")
 {
+    context.user.showConnecting();
 }
+
+    void ConnectingState::handleAttachAccept(){
+        context.timer.stopTimer();
+        context.setState<ConnectedState>();
+
+    }
+
+
 
 }
