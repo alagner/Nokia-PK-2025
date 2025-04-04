5c78e6e6966ecfee8cfdc5d2190118ce70e3ab36
WoyTuloO <wojciech.blicharz192@gmail.com>
2025-04-04 18:01:22 +0200

    updated ConnectedState.cpp/.hpp

--- src/UE/Application/States/ConnectedState.cpp
+++ src/UE/Application/States/ConnectedState.cpp
@@ -14,4 +14,13 @@ void ConnectedState::handleDisconnected(){
     context.setState<NotConnectedState>();
 }
 
+void ConnectedState::handleSmsReceive(common::PhoneNumber sender, std::string text){
+    logger.logInfo("SMS received from: ", sender, ", content : ", text);
+
+    std::size_t smsIndex = context.smsStorage.addMessage(sender, text);
+    logger.logDebug("SMS stored at index: ", smsIndex);
+
+    context.user.showNewMessage();
+}
+
 }
--- src/UE/Application/States/ConnectedState.hpp
+++ src/UE/Application/States/ConnectedState.hpp
@@ -10,6 +10,8 @@ class ConnectedState : public BaseState
 public:
     ConnectedState(Context& context);
     void handleDisconnected() override;
+
+    void handleSmsReceive(common::PhoneNumber sender, std::string text) override;
 };
 
 }
