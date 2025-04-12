c447e7ee197b003f29e66dfb76754f4dc38e2226
bargiee <bjgrzybowski@gmail.com>
2025-04-12 23:16:57 +0200

    Add handle methods for call

--- src/UE/Application/Ports/IBtsPort.hpp
+++ src/UE/Application/Ports/IBtsPort.hpp
@@ -18,6 +18,10 @@ public:
     virtual void handleDisconnect() = 0;
     virtual void handleSms(const common::PhoneNumber& from, const std::string& text) = 0;
     // Add handleUnknownRecipient later if needed
+    virtual void handleCallRequest(common::PhoneNumber from) = 0;
+    virtual void handleCallAccepted(common::PhoneNumber from) = 0;
+    virtual void handleCallDropped(common::PhoneNumber from) = 0;
+    virtual void handleCallTalk(common::PhoneNumber from, const std::string& text) = 0;
 };
 
 class IBtsPort
@@ -28,6 +32,13 @@ public:
     virtual void sendAttachRequest(common::BtsId) = 0;
     // Added for Sending SMS
     virtual void sendSms(const common::PhoneNumber& recipient, const std::string& text) = 0;
+
+    
+    virtual void sendCallRequest(const common::PhoneNumber& recipient) = 0;
+    virtual void sendCallAccepted(const common::PhoneNumber& recipient) = 0;
+    virtual void sendCallDropped(const common::PhoneNumber& recipient) = 0;
+    virtual void sendCallTalk(const common::PhoneNumber& recipient, const std::string& text) = 0;
+
 };
 
 } // namespace ue
