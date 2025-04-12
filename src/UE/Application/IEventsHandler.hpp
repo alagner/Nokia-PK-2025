94cdacca48f9424fa2686b5a66a8d401a5d5853d
bargiee <bjgrzybowski@gmail.com>
2025-04-12 20:36:28 +0200

    Add new call handling methods to IEventsHandler class

--- src/UE/Application/IEventsHandler.hpp
+++ src/UE/Application/IEventsHandler.hpp
@@ -3,6 +3,7 @@
 #include "Ports/ITimerPort.hpp"
 #include "Ports/IBtsPort.hpp"
 #include "Ports/IUserPort.hpp"
+#include <string>
 
 
 namespace ue
@@ -11,6 +12,13 @@ namespace ue
 class IEventsHandler : public IBtsEventsHandler,
                        public IUserEventsHandler,
                        public ITimerEventsHandler
-{};
+{
+  public:
+  virtual void handleCallRequest(common::PhoneNumber from) = 0;
+  virtual void handleCallAccepted(common::PhoneNumber from) = 0;
+  virtual void handleCallDropped(common::PhoneNumber from) = 0;
+  virtual void handleCallTalk(common::PhoneNumber from, const std::string& text) = 0;
+
+};
 
 }
