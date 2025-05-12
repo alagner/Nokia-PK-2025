d22f70dffda594cb49dcb84efd3b4ec285427109
Kamil <eskimosl@wp.pl>
2025-05-12 20:11:42 +0200

    test(init):  check initialization and initial state

---
+++ src/UE/Tests/Application/ApplicationBasicTestSuite.cpp
@@ -0,0 +1,93 @@
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+#include "Application.hpp"        
+#include "Mocks/ILoggerMock.hpp"
+#include "Mocks/IBtsPortMock.hpp"
+#include "Mocks/IUserPortMock.hpp"  
+#include "Mocks/ITimerPortMock.hpp"
+#include "Mocks/IUeGuiMock.hpp"     
+#include "Messages/PhoneNumber.hpp"
+#include "Messages/BtsId.hpp"       
+#include "Data/SmsData.hpp"         
+#include <memory>
+#include <chrono> 
+
+namespace ue
+{
+using namespace ::testing;
+
+// --- Definicje Mocków i Bazowej Fixture ---
+
+using LoggerNiceMock = NiceMock<common::ILoggerMock>;
+using BtsPortStrictMock = StrictMock<IBtsPortMock>;
+using UserPortStrictMock = StrictMock<IUserPortMock>;
+using TimerPortNiceMock = NiceMock<ITimerPortMock>;
+
+
+class ApplicationBasicTestFixture : public Test
+{
+protected:
+    const common::PhoneNumber PHONE_NUMBER{112}; 
+    const common::BtsId BTS_ID{1024};           
+    LoggerNiceMock loggerMock;
+    BtsPortStrictMock btsPortMock;
+    UserPortStrictMock userPortMock; 
+    TimerPortNiceMock timerPortMock;
+
+    
+    
+    IListViewModeMock listViewModeMock;
+    ISmsComposeModeMock smsComposeModeMock;
+    ITextModeMock viewTextModeMock;
+    IDialModeMock dialModeMock;
+    ICallModeMock callModeMock;
+    ITextModeMock alertModeMock;
+
+    Application objectUnderTest{PHONE_NUMBER,
+                                loggerMock,
+                                btsPortMock,
+                                userPortMock,
+                                timerPortMock};
+
+    ApplicationBasicTestFixture()
+    {
+    }
+
+    void simulateUserAction(const std::string& actionId) {
+        objectUnderTest.handleUserAction(actionId);
+    }
+
+    void ensureConnectedState()
+    {
+        EXPECT_CALL(btsPortMock, sendAttachRequest(BTS_ID));
+        EXPECT_CALL(timerPortMock, startTimer(std::chrono::milliseconds(500)));
+        EXPECT_CALL(userPortMock, showConnecting());
+        objectUnderTest.handleSib(BTS_ID);
+        Mock::VerifyAndClearExpectations(&btsPortMock);
+        Mock::VerifyAndClearExpectations(&timerPortMock);
+        Mock::VerifyAndClearExpectations(&userPortMock);
+
+        EXPECT_CALL(timerPortMock, stopTimer());
+        EXPECT_CALL(userPortMock, showConnected());
+        EXPECT_CALL(userPortMock, showNewSms(false));
+        objectUnderTest.handleAttachAccept();
+        Mock::VerifyAndClearExpectations(&timerPortMock);
+        Mock::VerifyAndClearExpectations(&userPortMock);
+    }
+};
+
+// test sprawdzający inicjalizację i stan początkowy
+TEST_F(ApplicationBasicTestFixture, shallInitializeInNotConnectedStateAndHandleSib)
+{
+    EXPECT_CALL(btsPortMock, sendAttachRequest(BTS_ID));
+    EXPECT_CALL(timerPortMock, startTimer(std::chrono::milliseconds(500))); 
+    EXPECT_CALL(userPortMock, showConnecting());
+
+    objectUnderTest.handleSib(BTS_ID); 
+
+}
+
+
+
+} // namespace ue
